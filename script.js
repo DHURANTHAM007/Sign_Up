f48a8f2478db8cf38ee2ce658fb5fb486078158e
DHURANTHAM007 <anjosh753@gmail.com>
2025-07-02 21:01:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function validateForm() {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password !== confirmPassword) {
-        alert('Passwords do not match!')
+function validateForm(){
+    const password =document.getElementById("password").value;
+    const confirmPassword= document.getElementById('confirmPassword').value;
+    if(password!==confirmPassword){
+        alert("Passwords do not match!")
     }
     else{
-        alert('Registration Successful!')
+        alert("Registration Successful!")
     }
+
+
 
 }
